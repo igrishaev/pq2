0a3d182819e14123b9dcff7f8df926cc18da86fc
Ivan Grishaev <ivan@grishaev.me>
2026-02-21 23:02:38 +0300

    updated

--- script/build-macos.sh
+++ script/build-macos.sh
@@ -13,6 +13,8 @@ JAVA_INC="-I${JAVA_HOME}/include -I${JAVA_HOME}/include/darwin"
 PLATFORM=$(cat PLATFORM)
 
 g++ -fPIC -I${LIBPQ}/include ${JAVA_INC} -c ${OBJ}.cpp -o ${OBJ}.o
-g++ -shared ${OBJ}.o -lpq -L${LIBPQ}/lib -o ${PLATFORM}.dylib
+g++ -shared ${OBJ}.o -lpq -L${LIBPQ}/lib -o ${PLATFORM}_api.dylib
 
-ls -l . | grep *.dylib
+ls -l ${LIBPQ}/lib
+
+cp ${LIBPQ}/lib/libpq.dylib ./${PLATFORM}_libpq.dylib
