817af670e39214a617ff647138a018f5b6ad21cd
Ivan Grishaev <ivan@grishaev.me>
2026-02-21 22:51:35 +0300

    add macos

--- script/build-macos.sh
+++ script/build-macos.sh
@@ -8,11 +8,11 @@ OBJ=org_pq_Native
 # remove traing slash
 JAVA_HOME=$(echo ${JAVA_HOME%/})
 
-JAVA_INC="-I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux"
+JAVA_INC="-I${JAVA_HOME}/include -I${JAVA_HOME}/include/darwin"
 
 PLATFORM=$(cat PLATFORM)
 
 g++ -fPIC -I$(pg_config --includedir) ${JAVA_INC} -c ${OBJ}.cpp -o ${OBJ}.o
-g++ -shared ${OBJ}.o -lpq -o ${PLATFORM}.so
+g++ -shared ${OBJ}.o -lpq -L$(pg_config --libdir) -o ${PLATFORM}.dylib
 
-ls -l . | grep *.so
+ls -l . | grep *.dylib
