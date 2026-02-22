76a5691806fc2e3370c6bd47f25098fe0ec58a57
Ivan Grishaev <ivan@grishaev.me>
2026-02-22 13:16:06 +0300

    updated

--- script/build-windows.sh
+++ script/build-windows.sh
@@ -13,13 +13,13 @@ JAVA_INC="-I${JAVA_HOME}/include -I${JAVA_HOME}/include/win32"
 PLATFORM=$(cat PLATFORM)
 
 g++ -fPIC ${JAVA_INC} -c ${OBJ}.cpp -o ${OBJ}.o
-g++ -shared ${OBJ}.o -lpq -o ${PLATFORM}.dll
+g++ -shared ${OBJ}.o -lpq -o ${PLATFORM}.dll.a
 
 MSYS_DIR=D:/a/_temp/msys64/${MSYS}
 
 ls -l ${MSYS_DIR}/lib
 
 # copy libpq
-cp ${MSYS_DIR}/lib/libpq.dll ./${PLATFORM}_libpq.dll
+cp ${MSYS_DIR}/lib/libpq.dll.a ./${PLATFORM}_libpq.dll.a
 
-find . -name '*.dll'
+find . -name '*.dll.a'
