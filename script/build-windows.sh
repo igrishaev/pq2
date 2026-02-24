79d775f6c5e77178e6f509a7acc02f63cf2e6f50
Ivan Grishaev <ivan@grishaev.me>
2026-02-24 21:05:54 +0300

    updated

--- script/build-windows.sh
+++ script/build-windows.sh
@@ -16,10 +16,10 @@ g++ -fPIC ${JAVA_INC} -c ${OBJ}.cpp -o ${OBJ}.o -march=x86-64
 g++ -shared ${OBJ}.o -lpq -o ${PLATFORM}_api.dll -march=x86-64
 
 # compile static libpq as shared
-# g++ -shared ${MSYS_DIR}/lib/libpq.dll.a -o ${PLATFORM}_libpq.dll
+g++ -shared ${MSYS_DIR}/lib/libpq.dll.a -o ${PLATFORM}_libpq.dll -march=x86-64
 
-g++ -shared -lpq -o ${PLATFORM}_libpq.dll -march=x86-64
+# g++ -shared -lpq -o ${PLATFORM}_libpq.dll -march=x86-64
 
 # ls -la ${MSYS_DIR}/lib
 
-find . -name '*.dll'
+ls -la '*.dll'
