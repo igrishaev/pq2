a69ca96f6c49c0f9eb2e83c74cedcd3fc57a6ab9
Ivan Grishaev <ivan@grishaev.me>
2026-02-24 18:42:04 +0300

    updated

--- script/build-windows.sh
+++ script/build-windows.sh
@@ -12,12 +12,14 @@ JAVA_INC="-I${JAVA_HOME}/include -I${JAVA_HOME}/include/win32"
 
 PLATFORM=$(cat PLATFORM)
 
-g++ -fPIC ${JAVA_INC} -c ${OBJ}.cpp -o ${OBJ}.o -lpq
-g++ -shared ${OBJ}.o -lpq -o ${PLATFORM}_api.dll
-
-ls -la ${MSYS_DIR}/lib
-
 # compile static libpq as shared
 g++ -shared ${MSYS_DIR}/lib/libpq.dll.a -o ${PLATFORM}_libpq.dll
+
+g++ -fPIC ${JAVA_INC} -c ${OBJ}.cpp -o ${OBJ}.o -lpq
+# g++ -shared ${OBJ}.o -lpq -o ${PLATFORM}_api.dll
+
+g++ -shared ${OBJ}.o -L. -lpq -o ${PLATFORM}_api.dll
+
+ls -la ${MSYS_DIR}/lib
 
 find . -name '*.dll'
