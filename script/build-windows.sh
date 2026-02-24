bbd60b23b5a5c33b56125675622791e6576270f1
Ivan Grishaev <ivan@grishaev.me>
2026-02-24 21:02:46 +0300

    updated

--- script/build-windows.sh
+++ script/build-windows.sh
@@ -12,13 +12,13 @@ JAVA_INC="-I${JAVA_HOME}/include -I${JAVA_HOME}/include/win32"
 
 PLATFORM=$(cat PLATFORM)
 
-g++ -fPIC ${JAVA_INC} -c ${OBJ}.cpp -o ${OBJ}.o -march=x86_64
-g++ -shared ${OBJ}.o -lpq -o ${PLATFORM}_api.dll -march=x86_64
+g++ -fPIC ${JAVA_INC} -c ${OBJ}.cpp -o ${OBJ}.o -march=x86-64
+g++ -shared ${OBJ}.o -lpq -o ${PLATFORM}_api.dll -march=x86-64
 
 # compile static libpq as shared
 # g++ -shared ${MSYS_DIR}/lib/libpq.dll.a -o ${PLATFORM}_libpq.dll
 
-g++ -shared -lpq -o ${PLATFORM}_libpq.dll -march=x86_64
+g++ -shared -lpq -o ${PLATFORM}_libpq.dll -march=x86-64
 
 # ls -la ${MSYS_DIR}/lib
 
